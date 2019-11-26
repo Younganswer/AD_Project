a358a2c1deec575d1c4bfe02764ea1f80753b0e9
Youngseo0810 <younganswer@kookmin.ac.kr>
2019-11-26 14:07:14 +0900

    .

---
+++ Help_Choose/Main/InitialScreen.py
@@ -0,0 +1,42 @@
+import sys
+import random
+from PyQt5.QtMultimedia import QSound
+from PyQt5.QtCore import (
+    Qt,
+    QBasicTimer
+)
+from PyQt5.QtGui import (
+    QBrush,
+    QPixmap
+)
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QPushButton,
+    QGridLayout,
+    QLabel,
+    QGraphicsItem,
+    QGraphicsPixmapItem,
+    QGraphicsRectItem,
+    QGraphicsScene,
+    QGraphicsView
+)
+
+from AskClose   import AskClose
+from BackGround import BackGround
+from Bullet     import Bullet
+from FoodInfo      import FoodInfo
+from Player     import Player
+
+SCREEN_WIDTH            = 800
+SCREEN_HEIGHT           = 600
+PLAYER_SPEED            = 3   # pix/frame
+PLAYER_BULLET_X_OFFSETS = [0, 45, 90]
+PLAYER_BULLET_Y         = 15
+BULLET_SPEED            = 10  # pix/frame
+BULLET_FRAMES           = 50
+FRAME_TIME_MS           = 16  # ms/frame
+# frame * ms/frame = ms  =>  frame(y) = whole_ms(x) / frame_ms(16)
+ENEMY_SPONE_X           = 800
+ENEMY_SPONE_Y           = 600
+ENEMY_FRAMES            = 500
